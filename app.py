fb5b7172b72b2f7e26d8d6152772d65ff027beed
Pavel <shmachilin@gmail.com>
2025-02-23 17:59:15 +0000

    Добавить маршрут для вычисления MD5 хеша строки

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-# https://proglib.io/p/samouchitel-po-python-dlya-nachinayushchih-chast-23-osnovy-veb-razrabotki-na-flask-2023-06-27
-from flask import Flask, render_template
+import hashlib
+import base64
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
@@ -14,6 +15,19 @@ def about():
 @app.route('/user/<int:user_id>')
 def user_profile(user_id):
     return f"Это профиль пользователя с ID {user_id}"
+
+@app.route('/md5', methods=['GET', 'POST'])
+def md5_hash():
+    if request.method == 'POST':
+        input_str = request.form.get('str', '')
+    else:
+        input_str = request.args.get('str', '')
+
+    if input_str:
+        md5_result = hashlib.md5(input_str.encode()).hexdigest()
+        return md5_result
+    else:
+        abort(400, description="Пожалуйста, предоставьте строку для вычисления MD5 хеша.")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
